eaa50848766ce232d06e6e1659418fe6c6d2db6f
Tomasz Gawron <tg29083@zpsb.pl>
2021-11-07 14:57:22 +0100

    cash, cashbag coverig

--- tomasz.gawron/tomasz.gawron/UnitTest1.cs
+++ tomasz.gawron/tomasz.gawron/UnitTest1.cs
@@ -1,13 +1,16 @@
 using NUnit.Framework;
 using System;
+using NUnit.Samples.Cash;
 
 namespace tomasz.gawron
 {
     public class Tests
     {
+        private Cash f14CHF;
         [SetUp]
         public void Setup()
         {
+            f14CHF = new Cash(14, "CHF");
             Console.WriteLine("This is setup");
         }
 
@@ -42,6 +45,48 @@ namespace tomasz.gawron
         {
             bool x = true;
             Assert.IsTrue(x);
+        }
+        [Test]
+        public void SimpleMultiply()
+        {
+            // [14 CHF] *2 == [28 CHF] 
+            Cash expected = new Cash(28, "CHF");
+            Assert.AreEqual(expected, f14CHF.Multiply(2));
+        }
+        [Test]
+        public void SimpleNegate()
+        {
+            Cash expected = new Cash(-14, "CHF");
+            Assert.AreEqual(expected, f14CHF.Negate());
+        }
+        [Test]
+        public void SimpleSubstract()
+        {
+            Cash expected = new Cash(0, "CHF");
+            Cash sub = new Cash(14, "CHF");
+
+            Assert.AreEqual(expected, f14CHF.Subtract(sub));
+        }
+
+        [Test]
+        public void SimpleAddMoney()
+        {
+            Cash expected = new Cash(28, "CHF");
+            Cash add = new Cash(14, "CHF");
+
+            Assert.AreEqual(expected, f14CHF.AddMoney(add));
+        }
+
+        [Test]
+        public void SimpleAddMoneyBag() {
+            Cash expected = new Cash(28, "CHF");
+            Cash add = new Cash(14, "CHF");
+            Cash add2 = new Cash(14, "CHF");
+            CashBag bag_big = new CashBag(expected, expected);
+            CashBag bag2 = new CashBag(add, add2);
+
+
+            Assert.AreEqual(bag_big, bag2.AddMoneyBag(bag2));
         }
     }
 }
