26ba61a7b2ddba604ec040377f5f2ff92cc2fdb4
Krzysztof Szadziun <krzysztofszadziun@gmail.com>
2021-11-07 14:42:50 +0100

    SecondTest

--- Krzysztof Szadziun/NUnitTestProject1/NUnitTestProject1/UnitTest1.cs
+++ Krzysztof Szadziun/NUnitTestProject1/NUnitTestProject1/UnitTest1.cs
@@ -1,4 +1,6 @@
 using NUnit.Framework;
+using System;
+using NUnit.Samples.Cash;
 
 namespace NUnitTestProject1
 {
@@ -7,12 +9,36 @@ namespace NUnitTestProject1
         [SetUp]
         public void Setup()
         {
+            Console.WriteLine("This is SetUp");
         }
 
+        [TearDown]
+        public void Teardown()
+        {
+            Console.WriteLine("This is TearDown");
+        }
+
+
         [Test]
+
+        [Category("Smoke")]
         public void Test1()
         {
-            Assert.Pass();
+            var x = 1;
+            var y = 1;
+            Assert.AreEqual(x, y);
         }
+        
+        [Test
+            ]
+        [Category("Sanity")]
+        public void Test2()
+        {
+            var a = 2;
+            var b = 2;
+            Assert.AreSame(a, b);
+        }
+        
+      
     }
 }
