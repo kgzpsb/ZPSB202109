6be40773f4199982547931d369b8af3d6340ab81
nkonsko <konskonatalia@gmail.com>
2021-11-19 17:53:11 +0100

    Zmiany3

--- Natalia_Konsko/TestProject1/UnitTest1.cs
+++ Natalia_Konsko/TestProject1/UnitTest1.cs
@@ -1,18 +1,35 @@
 using NUnit.Framework;
+using System;
 
 namespace TestProject1
 {
     public class Tests
     {
         [SetUp]
-        public void Setup()
+        public void SetUp()
         {
+            Console.WriteLine("This is SetUp");
         }
 
+        [TearDown]
+        public void Teardown()
+        {
+            Console.WriteLine("This is Teardown");
+        }
+        [Category("Sanity")]
         [Test]
         public void Test1()
         {
             Assert.Pass();
         }
+        [Category("Smoke")]
+        [Test]
+        public void Test2()
+        {
+            var x = 1;
+            var y = 2;
+            Assert.AreEqual(x, y);
+        }
+
     }
 }
