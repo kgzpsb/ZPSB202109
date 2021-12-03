a453663e27faa304af64da5ef7628dae4b9ae23f
Kacper Woźniak <kw28422@zpsb.pl>
2021-12-03 20:59:43 +0100

    Lab 3 Selenium finished up to 2. l (only first step made in m)

---
+++ Kacper.Wozniak/TestProject1/SpecFlowProject2/Steps/WPPLSteps.cs
@@ -0,0 +1,74 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Threading;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowProject2.Steps
+{
+    [Binding]
+    public class WPPLSteps
+    {
+        private IWebDriver webdriver;
+        private WebDriverWait webdriverWait;
+        public WPPLSteps(IWebDriver driver)
+        {
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            webdriver = driver;
+            webdriverWait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Given(@"I enter wp\.pl")]
+        public void GivenIEnterWp_Pl()
+        {
+            webdriver.Navigate().GoToUrl("http://www.wp.pl");
+            //wait for load
+            Thread.Sleep(5000);
+            var popup = "/html/body/div[2]/div/div[2]/div[3]/div/button[2]";
+            var element = webdriverWait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.XPath(popup)));
+            // bad practice
+            Thread.Sleep(5000);
+            element.Click();
+        }
+
+        [Given(@"I click on (.*)")]
+        public void GivenIClickOn(string p0)
+        {
+            // bad practice
+            Thread.Sleep(5000);
+            var firstXPath = "//*[@id="site - header"]/div[2]/div[4]/a[1]";
+            var elementPoczta = webdriver.FindElement(By.XPath(firstXPath));
+            elementPoczta.Click();
+        }
+
+        [When(@"I fill wrong email login")]
+        public void WhenIFillWrongEmailLogin()
+        {
+            var login = webdriver.FindElement(By.Id("login"));
+            login.SendKeys("Test");
+        }
+
+        [When(@"I fill wrong password")]
+        public void WhenIFillWrongPassword()
+        {
+            var pass = webdriver.FindElement(By.Name("password"));
+            pass.SendKeys("pomidor");
+        }
+
+        [When(@"I press submit")]
+        public void WhenIPressSubmit()
+        {
+            var submitBtnCssSelector = "#btnSubmit";
+            var submitBtn = webdriver.FindElement(By.CssSelector(submitBtnCssSelector));
+            Thread.Sleep(5000);
+            submitBtn.Click();
+            Thread.Sleep(5000);
+        }
+
+        [Then(@"I expect to see message as „Niestety podany login lub hasło jest błędne\.”")]
+        public void ThenIExpectToSeeMessageAsNiestetyPodanyLoginLubHasloJestBledne_()
+        {
+            //Zaimplementowac
+        }
+    }
+}
