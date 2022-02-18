8c4ebb5427a685b2e2e12425acad2cccda67faf3
Karol Pluciński <kp27089@zpsb.pl>
2022-02-18 13:07:55 +0100

    8h

---
+++ Karol.Plucinski/TestProject1/SpecFlowProject1/Steps/CashClassUnitTestingSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using NUnit.Samples.Cash;
+using NUnit.Framework;
+
+namespace SpecFlowProject1.Steps
+{
+    [Binding]
+    public class CashClassUnitTestingSteps
+    {
+        Cash oldCash;
+        Cash newCash;
+
+
+        [Given(@"Cash CHF (.*)")]
+        public void GivenCashCHF(int value)
+        {
+            oldCash = new Cash(value, "XXX");
+        }
+        
+        [When(@"Multipy by (.*)")]
+        public void WhenMultipyBy(int multiply)
+        {
+            newCash = (Cash)oldCash.Multiply(multiply);
+        }
+        
+        [Then(@"Cash Value is (.*)")]
+        public void ThenCashValueIs(int newValue)
+        {
+            Assert.AreEqual(newValue, newCash.Amount);
+        }
+    }
+}
