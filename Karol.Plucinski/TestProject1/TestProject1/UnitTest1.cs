c71c5c8e9771748e7f330dac686257d3a331ec13
Karol Pluciński <kp27089@zpsb.pl>
2022-02-18 09:56:48 +0100

    lab 1 do zad 4 i

--- Karol.Plucinski/TestProject1/TestProject1/UnitTest1.cs
+++ Karol.Plucinski/TestProject1/TestProject1/UnitTest1.cs
@@ -1,13 +1,16 @@
 using NUnit.Framework;
 using System;
+using NUnit.Samples.Cash;
 
 namespace TestProject1
 {
     public class Tests
     {
+        private Cash f14CHF;
         [SetUp]
         public void Setup()
         {
+            f14CHF = new Cash(14, "CHF");
             Console.WriteLine("This is SetUp");
         }
 
@@ -36,6 +39,18 @@ namespace TestProject1
         public void Teardown()
         {
             Console.WriteLine("This is Teardown");
+        }
+
+        /// <summary>
+        /// Assert that multiplying currency in Cash happens correctly
+        /// </summary>
+        ///
+        [Test]
+        public void SimpleMultiply()
+        {
+            // [14 CHF] *2 == [28 CHF]
+            Cash expected = new Cash(28, "CHF");
+            Assert.AreEqual(expected, f14CHF.Multiply(2));
         }
     }
 }
